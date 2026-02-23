399d9c8a6b42d611b47d6ae3b677f05ba2f92293
joseph-lins <josephvitor9@gmail.com>
2026-02-23 09:39:05 -0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,5 @@
+define('DB_HOST', getenv('DB_HOST') ?: 'mysql');
+define('DB_PORT', getenv('DB_PORT') ?: '3306');
+define('DB_NAME', getenv('DB_NAME') ?: 'demo_access');
+define('DB_USER', getenv('DB_USER') ?: 'root');
+define('DB_PASS', getenv('DB_PASS') ?: '');
